032db39a503abe902d068369428aa3cdc255ea7a
LukasD <lukas.dvarionis@gmail.com>
2022-06-21 16:58:43 +0300

    3.12: Command-line database

---
+++ mongo.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+}
+
+const password = process.argv[2];
+console.log(password)
+const url = `mongodb+srv://phonebook:${password}@cluster0.njpkrqz.mongodb.net/Phonebook?retryWrites=true&w=majority`;
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+if (process.argv.length < 4) {
+
+    mongoose
+    .connect(url)
+    .then(result => {
+        return Person.find({})
+    })
+    .then(persons => {
+        console.log("phonebook:")
+        persons.forEach( person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+    })
+    .catch(err => {
+        console.log(err);
+    })
+    .finally(() => {
+        mongoose.connection.close();
+    });
+}
+else {
+
+    const personName = process.argv[3];
+    const phone = process.argv[4] || "";
+
+    mongoose
+    .connect(url)
+    .then(result => {
+        console.log(result);
+        const newPerson = new Person({
+            name: personName,
+            number: phone
+        })
+        console.log(newPerson);
+        return newPerson.save();
+    })
+    .then(result => {
+        console.log("result of save",result);
+        return mongoose.connection.close();
+    })
+    .catch(err =>{
+        console.log(err);
+    })
+}
